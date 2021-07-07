562c0b5a1056893362d2c7aeb5f2c214c23efe3c
Nicholas Shorter <nickshorter@lordsidiousssq.local>
2021-07-06 20:54:48 -0500

    started setup and install.sh

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ohmyoled
+
+# Prerequistes:
+
+You will have to install git
+
+```
+#!/bin/bash
+sudo apt install git -y 
+```
+
+Clone the repo
+```
+git clone https://github.com/TheFinalJoke/ohmyoled.git
+```
+
+Run the Install Script
+```
+#!/bin/bash
+chmod 755 install.sh
+sudo ./install.sh
+```
--- install.sh
+++ install.sh
@@ -53,4 +53,70 @@ git pull
 
 make
 
+echo "Creating Systemd File"
+cat <<EOF >> /usr/lib/systemd/system/ohmyoled.service
+[Unit]
+Description=OhMyOLED Service
+
+[Service]
+ExecStart=/usr/bin/python3 $SOURCE_DIR/main.py 
+User=root
+WorkingDirectory=$SOURCE_DIR
+
+[Install]
+WantedBy=multi-user.target
+EOF
+ln -s /usr/lib/systemd/system/ohmyoled.service /etc/systemd/system/multi-user.target.wants/
+systemctl daemon-reload
+
+mkdir -p /etc/ohmyoled/
+
+echo "Created OhMyOled Config file to /etc/ohmyoled"
+cat <<EOF >> /etc/ohmyoled/ohmyoled.conf
+[basic]
+# Log Level for Python
+# Critcal = 50
+# Error = 40
+# Warning = 30
+# Info = 20
+# Debug = 10
+loglevel = 10
+# Tokens will have to be Supported used with Approriate Modules
+#### Not Supported Yet #####
+# sport_token = 'SPORT_TOKEN'
+##############################
+# Stock Token based in FINN TOKEN https://finnhub.io/docs/api/introduction
+# stock_api_token = YOUR_FINN_TOKEN
+
+# Open Weather Token in https://openweathermap.org/
+# open_weather_token = open_weather_token 
+
+[matrix]
+chain_length = 1
+parallel = 1
+# Brightness from 0-100
+brightness = 100
+oled_slowdown = 3
+
+[weather]
+Run=False
+# Build the city as Dallas, US, For more accurate data, use zipcode
+#city = 
+zipcode =  
+# Format
+# The Units, The options are standard, metric, imperial
+format = imperial
+
+[stock]
+Run=False
+
+# symbol=
+
+# Runs Quote data for stocks
+# quote=True
+
+# Historical and days need
+# historical=True
+# days_ago=30
+EOF
 echo "Installation complete"
