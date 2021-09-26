ad89517c51b3d3c78078c8dc5f65ceb4750e7b67
Nick Shorter <nickshorter888@gmail.con>
2021-09-26 09:20:16 -0500

    Changed formatting and added annotations

--- matrix/stock/stockmatrix.py
+++ matrix/stock/stockmatrix.py
@@ -13,31 +13,77 @@ class StockMatrix(Matrix):
         self.api = api
         self.logger = logger
     
-    async def poll_api(self):
+    async def poll_api(self) -> Stock:
         return Stock(await self.api.run())
 
-    def render_symbol(self, api):
+    def render_symbol(self, api) -> None:
         font = ImageFont.truetype("fonts/04B_03B_.TTF", 8)
-        self.draw_text((0, 0), f"{api.get_symbol.upper()}", font, fill=(0, 89, 255))
-        
-    def render_current_price(self, api):
+        self.draw_text(
+            (0, 0), 
+            f"{api.get_symbol.upper()}", 
+            font, 
+            fill=(0, 89, 255)
+        )
+    
+    def format_price(self, price: int) -> str:
+        return '{:<06}'.format(str(round(price, 2)))
+
+    def render_current_price(self, api) -> None:
         if api.get_quote_previous_close == api.get_quote_current_price:
-            self.draw_text((13, 8), f"${str(round(api.get_quote_current_price, 2))}", font=ImageFont.truetype("fonts/04B_03B_.TTF", 8))
+            self.draw_text(
+                (13, 8), 
+                f"${self.format_price(api.get_quote_current_price)}", 
+                font=ImageFont.truetype("fonts/04B_03B_.TTF", 8)
+            )
         elif api.get_quote_previous_close > api.get_quote_current_price:
-            self.draw_text((13, 8), f"${str(round(api.get_quote_current_price, 2))}", font=ImageFont.truetype("fonts/04B_03B_.TTF", 8), fill=(255,0,0))
+            self.draw_text(
+                (13, 8), 
+                f"${self.format_price(api.get_quote_current_price)}", 
+                font=ImageFont.truetype("fonts/04B_03B_.TTF", 8), 
+                fill=(255,0,0)
+            )
         else:
-            self.draw_text((13, 8), f"${str(round(api.get_quote_current_price, 2))}", font=ImageFont.truetype("fonts/04B_03B_.TTF", 8), fill=(0,255,0))
+            self.draw_text(
+                (13, 8), 
+                f"${self.format_price(api.get_quote_current_price)}", 
+                font=ImageFont.truetype("fonts/04B_03B_.TTF", 8), 
+                fill=(0,255,0)
+            )
 
         font = ImageFont.truetype("fonts/04B_03B_.TTF", 8)
         self.draw_text((1, 8), "P:", font=font)
-    def render_highest_price(self, api):
+
+    def render_highest_price(self, api) -> None:
         font = ImageFont.truetype('fonts/04B_03B_.TTF', 8)
-        self.draw_text((1, 11), "\uf058", font=ImageFont.truetype("fonts/weathericons.ttf", 11), fill=(0, 255, 0))
-        self.draw_text((13, 16), f"${round(api.get_quote_highest_price, 2)}", font=font, fill=(0,255,0))
-    def render_lowest_price(self, api):
-        self.draw_text((1, 19), "\uf044", font=ImageFont.truetype("fonts/weathericons.ttf", 11), fill=(255, 0, 0))
-        self.draw_text((13, 24), f"${round(api.get_quote_lowest_price, 2)}", font=ImageFont.truetype('fonts/04B_03B_.TTF', 8), fill=(255,0,0))
-    def render(self, api):
+        self.draw_text(
+            (1, 11), 
+            "\uf058", 
+            font=ImageFont.truetype("fonts/weathericons.ttf", 11), 
+            fill=(0, 255, 0)
+        )
+        self.draw_text(
+            (13, 16), 
+            f"${self.format_price(api.get_quote_current_price)}", 
+            font=font, 
+            fill=(0,255,0)
+        )
+    
+    def render_lowest_price(self, api) -> None:
+        self.draw_text(
+            (1, 19), 
+            "\uf044", 
+            font=ImageFont.truetype("fonts/weathericons.ttf", 11), 
+            fill=(255, 0, 0)
+        )
+        
+        self.draw_text(
+            (13, 24), 
+            f"${self.format_price(api.get_quote_current_price)}", 
+            font=ImageFont.truetype('fonts/04B_03B_.TTF', 8), 
+            fill=(255,0,0)
+        )
+    
+    def render(self, api) -> None:
         self.logger.info("Started Render for Stock Matrix")
         self.clear()
         self.reload_image()
