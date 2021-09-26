309ab9e47bf723a6b5312dd7ad99d6c2e2f65097
Nick Shorter <nickshorter888@gmail.con>
2021-09-26 14:58:05 -0500

    More options for time

--- matrix/time.py
+++ matrix/time.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 
 from PIL.Image import Image
-from matrix.matrix import Matrix, MatrixBase, FontException
-from datetime import date, datetime
-from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from matrix.matrix import MatrixBase
+from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
-import sys
+from enum import Enum
 import time 
 
+class TimeFormat(Enum):
+    TWELEVE = 1
+    TWENTYFOUR = 2
 class TimeMatrix(MatrixBase):
-    def __init__(self, matrix) -> None:
+    def __init__(self, matrix, config) -> None:
         super().__init__(matrix)
         self.matrix = matrix
+        self.config = config
         self.logger.debug("Time Matrix Initalized")
+    
     def return_time(self, fmt: str):
         return datetime.now().strftime(fmt) 
 
@@ -22,17 +26,21 @@ class TimeMatrix(MatrixBase):
         """
         self.logger.debug("No Api call reqiured for time module")
         return None
+    
+    def build_fmt(self):
+        return "%I:%M:%S %p" if TimeFormat.TWELEVE else "%H:%M:%S"  
+
     def render(self, poll):
         # Build something that Loads in corner for all the modules loaded
         self.logger.info("Running Module TimeMatrix")
         counter = 0
-        while counter < 15:
+        while counter < 30:
             self.logger.debug(f'Counter for module run {counter}')
             font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf", 10)
             self.set_image(Image.new("RGB", (64, 32)))
             self.set_draw(ImageDraw.Draw(self.get_image))
-            self.draw_text((3, 5), f"{self.return_time('%m/%d/%Y')}", font=font, fill=(71, 181, 214,255))
-            self.draw_text((8, 16), f"{self.return_time('%I:%M:%S')}", font=font, fill=(71, 181, 214,255))
+            self.draw_text((3, 5), f"{self.return_time('%m/%d/%Y')}", font=font, fill=eval(self.config.get('color')))
+            self.draw_text((8, 16), f"{self.return_time('%I:%M:%S')}", font=font, fill=eval(self.config.get('color')))
             self.render_image()
             counter = counter + 1
             time.sleep(1)
