b985f32313a66de9344a33ad45d93d3dbb657013
Nick Shorter <nickshorter888@gmail.con>
2021-10-05 18:29:33 -0500

    Updated ohmyoled conf

--- install.sh
+++ install.sh
@@ -40,43 +40,47 @@ cat <<EOF >> /etc/ohmyoled/ohmyoled.conf
 # Warning = 30
 # Info = 20
 # Debug = 10
-loglevel = 10
+loglevel = 20
 # Tokens will have to be Supported used with Approriate Modules
-#### Not Supported Yet #####
-# sport_token = 'SPORT_TOKEN'
-##############################
+# sport_token = 
 # Stock Token based in FINN TOKEN https://finnhub.io/docs/api/introduction
-# stock_api_token = YOUR_FINN_TOKEN
+# stock_api_token = 
 
 # Open Weather Token in https://openweathermap.org/
-# open_weather_token = open_weather_token 
+# open_weather_token =  
 
 [matrix]
 chain_length = 1
 parallel = 1
 # Brightness from 0-100
-brightness = 100
+brightness = 20
 oled_slowdown = 3
+fail_on_error = False
+
+[time]
+Run=True
+color = (192,192,192)
 
 [weather]
-Run=False
+Run = False
 # Build the city as Dallas, US, For more accurate data, use zipcode
+current_location=True
 #city = 
-zipcode =  
+# zipcode =  
 # Format
 # The Units, The options are standard, metric, imperial
 format = imperial
 
 [stock]
 Run=False
-
 # symbol=
 
-# Runs Quote data for stocks
-# quote=True
-############## Not Supported yet ##################
-# Historical and days need
-# historical=True
-# days_ago=30
-###################################################
+[sport]
+Run=False
+# Choose: football baseball basketball
+# sport=
+# For Team IDs Baseball https://dashboard.api-football.com/baseball/ids/teams
+# For Team IDs Basketball https://dashboard.api-football.com/basketball/ids/teams/USA
+# Only Support USA
+# team_id = 
 EOF
