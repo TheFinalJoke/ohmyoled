243bdedb6961ed985ea6f49a132edf21f20a4dbd
Nick Shorter <nickshorter888@gmail.con>
2022-01-08 21:12:34 -0600

    fixed install.sh script

--- install.sh
+++ install.sh
@@ -6,15 +6,20 @@ then
 echo "Are you root?"
 exit 2
 fi
-apt update && apt upgrade -y
-apt install -y fonts-noto-mono
+apt update && apt upgrade -y && apt install -y fonts-noto-mono
+if [ 0 == $(echo $?) ]
+then
+echo "Error Occured While installing and updatings fonts"
+exit 1
+fi 
 
 mv -v $SOURCE_DIR/fonts/* /usr/share/fonts/
 mv -v $SOURCE_DIR/ohmyoled /usr/local/bin/
-if [[ -f /usr/lib/systemd/system/ohmyoled.service ]]
+if [ -f /usr/lib/systemd/system/ohmyoled.service ]
+then
 echo "ohmyoled service exists"
 else
-echo "Creating Systemd File"
+echo "Creating Systemd File for ohmyoled"
 cat <<EOF >> /usr/lib/systemd/system/ohmyoled.service
 [Unit]
 Description=OhMyOLED Service
@@ -27,21 +32,21 @@ WorkingDirectory=/usr/local/bin/
 [Install]
 WantedBy=multi-user.target
 EOF
-fi 
-
-if 
+fi
+ 
 ln -s /usr/lib/systemd/system/ohmyoled.service /etc/systemd/system/multi-user.target.wants/
 systemctl daemon-reload
 
-mkdir -p /etc/ohmyoled/
-if [[ -f /etc/ohmyoled/ecIcons_utf8.csv ]] then 
-echo "Icons File already Exists"
-else
-mv -v $SOURCE_DIR/ecIcons_utf8.csv /etc/ohmyoled/
-fi
+mkdir -p /etc/ohmyoled/ && mkdir -p /var/log/ohmyoled/
+if [ 0 == $(echo $?) ]
+then
+echo "Error Occured creating ohmyoled dirs"
+exit 1
+fi 
 
 echo "Created OhMyOled Config file to /etc/ohmyoled"
-if [[ -f /etc/ohmyoled/ohmyoled.conf ]] then 
+if [ -f /etc/ohmyoled/ohmyoled.conf ] 
+then
 echo "/etc/ohmyoled/ohmyoled.conf already exists"
 else
 mv -r $SOURCE_DIR/ohmyoled.conf /etc/ohmyoled/
