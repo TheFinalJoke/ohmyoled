f2abf708600d876a3340764fcc475cf2737c1bd7
Nick Shorter <nickshorter888@gmail.com>
2022-01-21 16:54:14 -0600

    finished build_docker

--- builddocker.sh
+++ builddocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build container_build -t ohmyoled_container
+docker build container_build -t ohmyoled
 
 if [[ echo $? != 0 ]];
 then 
@@ -9,3 +9,11 @@ exit 1
 fi 
 
 echo "Succesfully built docker image"
+
+echo "Login to docker hub"
+docker login
+
+echo "Push the commit to docker hub"
+
+echo "Example: docker commit 0274fdd87f06 thefinaljoke/ohmyoled:dev"
+echo "Example Push: docker push thefinaljoke/ohmyoled:1.3.4"
